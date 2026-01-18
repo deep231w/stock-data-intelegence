17a76b58e0f32b55bc3828a4aa92beaf8c3382fd
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2026-01-18 15:22:16 +0530

    fetch companies added

--- backend/app/api/routes/companies.py
+++ backend/app/api/routes/companies.py
@@ -1,12 +1,12 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter ,Depends
+from sqlalchemy.orm import Session
+from app.core.db import get_db
+from app.models.models import Company
 router= APIRouter(prefix="/companies" , tags=["companies"])
 
 @router.get("/")
-def get_companies ():
-    return [
-        "tcs",
-        "hdfc",
-        "mahindra",
-        "google"
-        ]
+def get_companies (db:Session =Depends(get_db)):
+    companies = db.query(Company).all()
+    print("companies in fetch companies router- ", companies)
+    
+    return companies
