d3eb841c9bfc293d8a254498abde49f574de81a5
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2026-01-20 02:44:58 +0530

    updated cors url

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://stock-data-intelegence-pm2z.vercel.app/"
+        "https://stock-data-intelegence-pm2z.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
