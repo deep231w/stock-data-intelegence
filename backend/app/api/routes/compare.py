b6c516a8ad0b98b906af5149c671814f2b06e1dc
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2026-01-19 23:50:47 +0530

    update compare bet 2 companies

--- backend/app/api/routes/compare.py
+++ backend/app/api/routes/compare.py
@@ -2,6 +2,9 @@ from fastapi import APIRouter, Depends, HTTPException
 from app.models.models import Company, StockPrice
 from sqlalchemy.orm import Session
 from app.core.db import get_db
+from sqlalchemy import func
+import math
+
 router = APIRouter(prefix="/compare",tags=["compare between 2 companies"])
 
 def calculate_metrics(prices):
@@ -39,8 +42,47 @@ def compare_bet_2_companies(symbol1:str, symbol2:str, db:Session=Depends(get_db)
         )
         if not company1 or not company2:
             raise HTTPException(status_code=404, detail="Company not found")
+        
+        prices1 = (
+            db.query(StockPrice)
+            .filter(StockPrice.company_id == company1.id)
+            .order_by(StockPrice.date.desc())
+            .limit(30)
+            .all()
+        )
 
-        return {symbol1 , symbol2}
+        prices2 = (
+            db.query(StockPrice)
+            .filter(StockPrice.company_id == company2.id)
+            .order_by(StockPrice.date.desc())
+            .limit(30)
+            .all()
+        )
+
+        if len(prices1) < 2 or len(prices2) < 2:
+            raise HTTPException(status_code=400, detail="Not enough data")
+
+        ret1, vol1 = calculate_metrics(prices1)
+        ret2, vol2 = calculate_metrics(prices2)
+
+
+        return {
+            "period": "last_30_days",
+            "stock_1": {
+                "symbol": symbol1,
+                "return_percent": ret1,
+                "volatility": vol1
+            },
+            "stock_2": {
+                "symbol": symbol2,
+                "return_percent": ret2,
+                "volatility": vol2
+            },
+            "comparison": {
+                "better_return": symbol1 if ret1 > ret2 else symbol2,
+                "more_volatile": symbol1 if vol1 > vol2 else symbol2
+            }
+        }
     except ConnectionError as e:
         raise HTTPException(status_code=500 , detail="db connetion failed") from e
     except Exception as e:
